622beca433a34ca5eb55993201123cf09a1a0228
Jesus-S17 <122305215+Jesus-S17@users.noreply.github.com>
2023-02-17 10:27:52 -0800

    started whileLoops

---
+++ whileLoops1.js
@@ -0,0 +1,30 @@
+const readLine =  require('readline-sync');
+
+//1
+let x = 0;
+while(x<=25){
+  console.log(x);
+ x++;
+}
+//2
+let y = 0;
+while(y<=50){
+    console.log(y);
+    y++;
+}
+
+//3
+let z = -10;
+while(z<=10){
+    console.log(z);
+    z++;
+}
+
+//4
+let a = 0;
+while(a>-51){
+    console.log(a);
+    a--;
+}
+
+//5
