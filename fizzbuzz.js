ff75984d3d4d64c793675ff331cd199ece7eac31
Jesus-S17 <122305215+Jesus-S17@users.noreply.github.com>
2023-03-07 10:29:14 -0800

    Started fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,18 @@
+
+for(let i = 0; i<= 100; i++) {
+    if (i % 3 == 0)
+    {
+        console.log('Fizz');
+    }
+    if (i % 5 == 0)
+    {
+        console.log('Buzz');
+    }
+    if (i % 15 == 0)
+    {
+        console.log('Fizzbuzz');
+    }
+    if (i % 1 == 0)
+    {
+        console.log(i)
+    }}
